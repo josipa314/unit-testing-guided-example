53c704b59af791a74b8b1fa27d98bd7071264ab8
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-04-08 21:20:48 +0200

    unit testing done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,41 @@
 function sum(a, b) {
-  return;
+  if (a == undefined && b == undefined){
+    return 0;
+  }
+  else if (a== undefined) {
+    return b-0;
+  }
+  else if (b == undefined ) {
+    return a-0;
+  }
+  else {
+    return a + b;
+  }
+  
 }
 
 function subtract(a, b) {
-  return;
+  if (a == undefined && b == undefined){
+    return 0;
+  }
+  else if (a== undefined) {
+    return b-0;
+  }
+  else if (b == undefined ) {
+    return a-0;
+  }
+  else {
+    return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (b == 0) {
+    throw new Error('An explanatory error message');;
+  }
+  else {
+    return a/b;
+  } 
 }
 
 function multiply(a, b) {
